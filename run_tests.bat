de15ed100df2391ada5ae1e182528a5d0f7afe72
Arindam Mondal <arindam03215@gmail.com>
2025-06-13 23:30:24 +0530

    Fix run_tests.bat and tox.ini to correctly generate and open Allure reports

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 pytest -v -s -m Purchase --alluredir=Python_BDD_Framwork\reports\allure-results
-@REM allure generate Python_BDD_Framwork\reports\allure-results -o Python_BDD_Framwork\reports\allure-report --clean
-allure open Python_BDD_Framwork\reports\allure-report
+allure generate Python_BDD_Framwork\reports\allure-results -o Python_BDD_Framwork\reports\allure-report --clean
+@REM allure open Python_BDD_Framwork\reports\allure-report
