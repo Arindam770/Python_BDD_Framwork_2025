e45d29a926dc00b366ec8d95ae098d8b91ebb8f1
Arindam Mondal <arindam03215@gmail.com>
2025-06-13 20:40:03 +0530

    Build Management:- Adding Tox related Update

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
-pytest -v -s --alluredir=Python_BDD_Framwork\reports\allure-results
+pytest -v -s -m Purchase --alluredir=Python_BDD_Framwork\reports\allure-results
 @REM allure generate Python_BDD_Framwork\reports\allure-results -o Python_BDD_Framwork\reports\allure-report --clean
 allure open Python_BDD_Framwork\reports\allure-report
